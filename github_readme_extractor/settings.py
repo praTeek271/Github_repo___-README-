409200660984d473c1a603ca8cdf7fd8f0fa5754
prateek271 <prateekpatriotasme@gmail.com>
2023-08-09 12:04:27 +0530

    deploy fixes

--- github_readme_extractor/settings.py
+++ github_readme_extractor/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# allowed host for all 
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
