e0f6a30e3352ce999e52f4b889ec8cdd57572f3f
prateek271 <prateekpatriotasme@gmail.com>
2023-08-09 12:00:27 +0530

    deploy patches

--- github_readme_extractor/settings.py
+++ github_readme_extractor/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1)y2dwn5p#v^4s5ec8n_j!4p^l7a1*089m48#=1_hsb+i%th&f'
+# SECRET_KEY = 'django-insecure-1)y2dwn5p#v^4s5ec8n_j!4p^l7a1*089m48#=1_hsb+i%th&f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
